bf2f746359d366cf28c9ec36b63f2c3097e25aca
IanHe <yinghe.developer@gmail.com>
2020-08-13 07:10:12 -0700

    add binary tree traversal

--- 07_DFS&BFS/src/Solution_07.java
+++ 07_DFS&BFS/src/Solution_07.java
@@ -1,5 +1,53 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Solution_07 {
     public static void main(String[] args) {
 
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>() {{
+            add(root);
+        }};
+        while (!queue.isEmpty()) {
+            List<Integer> level = new LinkedList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node != null) {
+                    level.add(node.val);
+                    if (node.left != null) queue.add(node.left);
+                    if (node.right != null) queue.add(node.right);
+                }
+            }
+            if (!level.isEmpty()) {
+                result.add(level);
+            }
+        }
+        return result;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
