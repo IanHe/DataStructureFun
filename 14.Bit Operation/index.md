d88e4a00bdb23fd865b2289742274fb09694e208
IanHe <yinghe.developer@gmail.com>
2020-08-29 03:44:24 -0700

    complete bit operation

--- 14.Bit Operation/index.md
+++ 14.Bit Operation/index.md
@@ -1,1 +1,10 @@
 ##Bit Operation
+![Image of why_bit_operation](imgs/why_bit_operation.jpg)
+![Image of bit_operation](imgs/bit_operation.jpg)
+![Image of bit_operation_1](imgs/bit_operation_1.jpg)
+####1s(全1 - all 1)
+![Image of xor](imgs/xor.jpg)
+![Image of bit_operation_typicals](imgs/bit_operation_typical.jpg)
+![Image of bit_operation_useful](imgs/bit_operation_useful.jpg)
+###why n & (n-1) will remove the last 1
+![Image of n&_n-1](imgs/n&_n-1.jpg)
--- 14.Bit Operation/src/Solution_14.java
+++ 14.Bit Operation/src/Solution_14.java
@@ -1,5 +1,24 @@
 public class Solution_14 {
+
+
     public static void main(String[] args) {
+        Solution_14 solution = new Solution_14();
 
     }
+
+    public int[] countBits(int num) {
+        int[] result = new int[num+1];
+        for(int i = 0;i<=num;i++){
+            result[i] = numOfBit(i);
+        }
+        return result;
+    }
+
+    private int numOfBit(int x) {
+        int count = 0;
+        for (; x != 0; count++) x &= x - 1;
+        return count;
+    }
+
+
 }
