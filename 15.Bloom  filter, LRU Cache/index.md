bc7f4b7dd2cfe15d2807ff81bb12e3f8d94d7a54
IanHe <yinghe.developer@gmail.com>
2020-08-29 06:19:14 -0700

    complete Bloom filter & LRU Cache

--- 15.Bloom  filter, LRU Cache/index.md
+++ 15.Bloom  filter, LRU Cache/index.md
@@ -3,3 +3,22 @@
 ###Traditional Way of Hash Table Storage
 ![Image of /hashtable_storage](imgs//hashtable_storage.jpg)
 <br></br>
+###Bloom filter is an advanced search which only tells if element may exists or not exists
+###it is only used in the most-outer layer as a quick search cache
+![Image of /bloom_filter](imgs//bloom_filter.jpg)
+###Bloom filter uses bits to store info
+![Image of /bloom_filter_1](imgs//bloom_filter_1.jpg)
+###when find a item in the bloom filter which means it may exist, if not found means it is not in the filter 100%, in reality, if bloom filter find the search item, then the system will continue to search in the DB or other storage system
+![Image of /bloom_filter_2](imgs//bloom_filter_2.jpg)
+###Bloom filter usage
+![Image of /bloom_filter_usage](imgs//bloom_filter_usage.jpg)
+###Simple Bloom Filter - Java Implementation(refer to class: BloomFilter - https://github.com/lovasoa/bloomfilter/blob/master/src/main/java/BloomFilter.java)
+<br></br>
+<br></br>
+##LRU Cache (LRU - least recently used)
+![Image of /cpu_socket](imgs//cpu_socket.jpg)
+###LRU Cache Implementation - HashTable + Double LinkedList
+![Image of /lru_cache](imgs//lru_cache.jpg)
+###LRU Cache Insert - How does it work - insert A -> B -> C -> D -> E -> F -> C -> G
+![Image of /lru_cache_insert](imgs//lru_cache_insert.jpg)
+![Image of /lru_strategy](imgs//lru_strategy.jpg)
--- 15.Bloom  filter, LRU Cache/src/Solution_15.java
+++ 15.Bloom  filter, LRU Cache/src/Solution_15.java
@@ -1,5 +1,89 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution_15 {
     public static void main(String[] args) {
 
+    }
+
+
+}
+
+class LRUCache {
+    private Map<Integer, LinkNode> cache = new HashMap<>();
+    private int size, capacity;
+    private LinkNode head, tail;
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        //false head, tail
+        head = new LinkNode();
+        tail = new LinkNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        LinkNode node = cache.get(key);
+        if (node == null) return -1;
+        //because node is being searched, move the node to head
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value){
+        LinkNode node = cache.get(key);
+        if(node == null){
+            LinkNode newNode = new LinkNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            ++size;
+            if(size > capacity){
+                LinkNode tail = removeTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        }else{
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    private void moveToHead(LinkNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private void addToHead(LinkNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(LinkNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private LinkNode removeTail() {
+        //tails if false node, so the real tail the tail.prev
+        LinkNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    class LinkNode {
+        int key;
+        int value;
+        LinkNode prev, next;
+
+        public LinkNode() {
+        }
+
+        public LinkNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
