5075b60e0206ca21269d3e78a871071ee2b273ba
IanHe <yinghe.developer@gmail.com>
2020-08-30 07:12:34 -0700

    add one

--- 16.Sorting Algorithm/index.md
+++ 16.Sorting Algorithm/index.md
@@ -1,4 +1,7 @@
 ##Sorting Algorithm
+####https://www.cnblogs.com/onepixel/p/7674659.html
+####https://www.bilibili.com/video/av25136272
+####https://www.bilibili.com/video/av63851336
 <br></br>
 ###Sorting Algorithm - Classfication
 ![Image of /sorting_classification](imgs//sorting_classification.jpg)
@@ -15,3 +18,14 @@
 ![Image of /quick_sort_1](imgs//quick_sort_1.jpg)
 ###Merge Sort
 ![Image of /quick_sort](imgs//merge_sort.jpg)
+![Image of /quick_sort_1](imgs//merge_sort_1.jpg)
+![Image of /quick_sort_2](imgs//merge_sort_2.jpg)
+###Quick Sort vs Merge Sort
+![Image of /quick_vs_merge_sort](imgs//quick_vs_merge_sort.jpg)
+<br></br>
+##Heap Sort
+![Image of /heap_sort](imgs//heap_sort.jpg)
+![Image of /heap_sort_1](imgs//heap_sort_1.jpg)
+<br></br>
+##Special Sorting
+![Image of /special_sorting](imgs//special_sorting.jpg)
--- 16.Sorting Algorithm/src/Solution_16.java
+++ 16.Sorting Algorithm/src/Solution_16.java
@@ -2,4 +2,30 @@ public class Solution_16 {
     public static void main(String[] args) {
 
     }
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        // arr1 is between 1 ~ 1000, which is quite limited, so we can use bucket sort
+        int[] bucket = new int[1001];
+        //traverse arr1, and put into bucket
+        for(int num: arr1){
+            bucket[num]++;
+        }
+        //arr1 has been traversed, so we can utilize arr1 to store the final result to save space
+        int i = 0;
+        //traverse arr2, when num of arr2 exists in arr1, put into new arr1
+        for(int num: arr2){
+            while(bucket[num]-- > 0){
+                arr1[i++] = num;
+            }
+        }
+        //the rest of numbers in bucket does not exist in arr2, traverse bucket and put into arr1
+        for(int j=0;j<1001;j++){
+            while(bucket[j]-- > 0){
+                arr1[i++] =j;
+            }
+        }
+        return arr1;
+    }
 }
+
+
