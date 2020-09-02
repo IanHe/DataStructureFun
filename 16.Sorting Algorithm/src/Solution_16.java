9e51f8ad9b434f5a13ade81b3c23c9bac0f6c518
IanHe <yinghe.developer@gmail.com>
2020-09-02 07:33:07 -0700

    add more in sorting algorithm

--- 16.Sorting Algorithm/src/Solution_16.java
+++ 16.Sorting Algorithm/src/Solution_16.java
@@ -1,30 +1,26 @@
+import java.util.Arrays;
+
 public class Solution_16 {
     public static void main(String[] args) {
 
     }
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        // arr1 is between 1 ~ 1000, which is quite limited, so we can use bucket sort
-        int[] bucket = new int[1001];
-        //traverse arr1, and put into bucket
-        for(int num: arr1){
-            bucket[num]++;
-        }
-        //arr1 has been traversed, so we can utilize arr1 to store the final result to save space
-        int i = 0;
-        //traverse arr2, when num of arr2 exists in arr1, put into new arr1
-        for(int num: arr2){
-            while(bucket[num]-- > 0){
-                arr1[i++] = num;
+    public int[][] merge(int[][] intervals) {
+        // sort array, let it be like [[0,2], [0, 1], [1, 2], [1, 3], ...[99, 100] ]
+        Arrays.sort(intervals, (arr1, arr2) -> (arr1[0] - arr2[0]));
+        //result
+        int[][] result = new int[intervals.length][2];
+        int i = -1;
+        for (int[] interval : intervals) {
+            // when index start or the next one's first element is greater than the current one's last element, add new item
+            if (i == -1 || result[i][1] < interval[0]) {
+                result[++i] = interval;
+            } else {
+                // merge the next
+                result[i][1] = Math.max(result[i][1], interval[1]);
             }
         }
-        //the rest of numbers in bucket does not exist in arr2, traverse bucket and put into arr1
-        for(int j=0;j<1001;j++){
-            while(bucket[j]-- > 0){
-                arr1[i++] =j;
-            }
-        }
-        return arr1;
+        return Arrays.copyOf(result, i + 1);
     }
 }
 
