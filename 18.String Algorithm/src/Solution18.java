19b06c9baaa6ecceab4a9a43934343b60b0afc66
IanHe <yinghe.developer@gmail.com>
2020-09-18 18:19:22 -0700

    string algorithm

--- 18.String Algorithm/src/Solution18.java
+++ 18.String Algorithm/src/Solution18.java
@@ -1,5 +1,38 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Solution18 {
     public static void main(String[] args) {
+        Solution18 solution = new Solution18();
+        System.out.println(solution.reverseWords("    "));
+    }
 
+    /*
+        word begin: if(prev == ' ' and cur ! = ' ' or cur != ' ' && cur index = 0) wordBegin = wordEnd = cur Index
+        loop when cur != ' ' endIndex++
+        word end: cur == ' '
+     */
+    public String reverseWords(String s) {
+        Deque<String> deque = new ArrayDeque<>();
+        int wordBegin = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 && s.charAt(i) == ' ') continue;
+            // word begin
+            if (i == 0 && s.charAt(i) != ' ' || s.charAt(i - 1) == ' ' && s.charAt(i) != ' ') {
+                wordBegin = i;
+            }
+            // word end
+            if ( (s.charAt(i) == ' ' && s.charAt(i - 1) != ' ')) {
+                deque.offerFirst(s.substring(wordBegin, i));
+                wordBegin = -1;
+            }
+            // s end
+            if(i == s.length()-1 && wordBegin >= 0) {
+                deque.offerFirst(s.substring(wordBegin));
+            }
+
+        }
+
+        return String.join(" ", deque);
     }
 }
