9e7b2bb46d168c5be20757f6067d7131270cb0dd
IanHe <yinghe.developer@gmail.com>
2020-09-25 19:39:41 -0700

    more on arrays

--- 01.Arrays LinkedList SkipList/src/Solution_1.java
+++ 01.Arrays LinkedList SkipList/src/Solution_1.java
@@ -1,34 +1,24 @@
 public class Solution_1 {
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        printNode(swapPairs(node1));
+
     }
 
-    public static void printNode(ListNode head) {
-        while (head != null) {
-            System.out.print(head.val + " -> ");
-            head = head.next;
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
         }
-        System.out.println();
-    }
-
-    public static ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) return head;
-        ListNode next = head.next;
-        // skip one, do the next one's recursion
-        head.next = swapPairs(next.next);
-        next.next = head;
-        return next;
     }
 
     static class ListNode {
