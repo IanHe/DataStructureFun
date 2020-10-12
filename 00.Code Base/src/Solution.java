45388064fc3cad682f8abef439de470ad6f5d2c4
IanHe <yinghe.developer@gmail.com>
2020-10-12 06:53:37 -0700

    update trie & union_find

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,21 +1,83 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxProfit(new int[]{1,2,3,4,5}));
+        char[][] charArr = new char[1][2];
+        charArr[0][0] ='a';
+        charArr[0][1] ='a';
+        solution.findWords(charArr, new String[]{"aaa"}).forEach(System.out::println);
     }
 
-    //DP function:
-    // 0: not hold the stock, 1: hold the stock
-    // F(n)(0) = max(F(n-1)(0), F(n-1)(1) + a(n))
-    // F(n)(1) = max(F(n-1)(1), F(n-1)(0) - a(n))
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 1) return 0;
-        int dp0 = 0, dp1 = -prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            dp0 = Math.max(dp0, dp1 + prices[i]);
-            // because you can only buy the stock at most once, buy => -prices[i]
-            dp1 = Math.max(dp1, dp0 - prices[i]);
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        List<String> result = new ArrayList<>();
+        if (board == null || board.length == 0) return result;
+
+        //store words in Trie
+        for (String word : words) trie.insert(word);
+
+        //traverse each cell
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                dfs_backtrack(board, i, j, trie.root, result);
+            }
         }
-        return dp0;
+        return result;
+    }
+
+    private void dfs_backtrack(char[][] board, int x, int y, TrieNode node, List<String> result) {
+        //terminator
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
+        char ch = board[x][y];
+        // ch == '$' means ch has been visited before
+        if (ch == '$' || node.children[ch - 'a'] == null) return;
+
+        node = node.children[ch - 'a'];
+        //if found a word
+        if (node.word != null) {
+            result.add(node.word);
+            node.word = null;
+        }
+        //traverse four directions, set current to be '$', otherwise child node will traverse back
+        board[x][y] = '$';
+        dfs_backtrack(board, x - 1, y, node, result);
+        dfs_backtrack(board, x + 1, y, node, result);
+        dfs_backtrack(board, x, y - 1, node, result);
+        dfs_backtrack(board, x, y + 1, node, result);
+
+        //set state back
+        board[x][y] = ch;
+    }
+
+    static class TrieNode {
+        public TrieNode[] children;
+        public String word; // store word for leaf node
+
+        public TrieNode() {
+            children = new TrieNode[26];
+        }
+    }
+
+    static class Trie {
+        TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        public void insert(String word) {
+            TrieNode node = root;
+            for (char ch : word.toCharArray()) {
+                if (node.children[ch - 'a'] == null) {
+                    node.children[ch - 'a'] = new TrieNode();
+                }
+                node = node.children[ch - 'a'];
+            }
+            //store word on leaf node
+            node.word = word;
+        }
     }
 }
--- 11.Trie & Disjoint Sets/index.md
+++ 11.Trie & Disjoint Sets/index.md
@@ -1,16 +1,40 @@
-##Trie & Disjoint Sets(Union-Find)
+##Trie(前缀树/字典树) & Disjoint Sets(Union-Find)
+##Tree Structure - DFS vs BFS
+![Image of tree](imgs/tree.jpg)
+##Binary Tree - sub nodes on the left < node < sub nodes on the right
+![Image of binary_tree](imgs/binary_tree.jpg)
+##Binary Tree Data Structure is not able to handle search engine, we need new Data Structure - Trie
+![Image of search_engine](imgs/search_engine.jpg)
 ##Trie(字典树)
+####E.g: search "ten" -> search "t" -> search "e" -> search "n" => the path will form "ten" 
 ![Image of trie](imgs/trie.jpg)
 ###每个节点可能存储额外的信息，例如单词搜频率
 ![Image of trie_attribute](imgs/trie_attribute.jpg)
 ![Image of trie_internal](imgs/trie_internal.jpg)
+###Trie - Core Think - which natively solves search engine problem
+![Image of trie_core](imgs/trie_core.jpg)
+##Leetcode:
+####208. Implement Trie (Prefix Tree) - medium - https://leetcode.com/problems/implement-trie-prefix-tree/
+####212. Word Search II - hard - https://leetcode.com/problems/word-search-ii/
+
+
+<br></br>
+<br></br>
 ##Disjoint Sets(并查集) - Group or not?(组团，配对的问题)
 ![Image of disjoint_sets](imgs/disjoint_sets.jpg)
-###initialize each element with parent element as itself
+##Example - friends-circles, number-of-islands
+####547. Friend Circles - medium - https://leetcode.com/problems/friend-circles/
+####200. Number of Islands - medium - https://leetcode.com/problems/number-of-islands/
+####0, 1, 2 represent 3 persons, f(0, 1) = 1 => 0 and 1 is in the same set
+![Image of find_union_example](imgs/find_union_example.jpg)
+####https://leetcode-cn.com/problems/surrounded-regions/
+<br></br>
+###Initialize each element with parent element point to itself -> the node itself is the parent
 ![Image of disjoint_sets_init](imgs/disjoint_sets_init.jpg)
-###Search and Combine
+###Search: find the leading(parent) node of the set
+###Combine: parent(e) -> a or parent(a) -> e
 ![Image of disjoint_sets_combine](imgs/disjoint_sets_combine.jpg)
-###Can optimize search by path compression
+###Can optimize search by path compression: point all sub nodes direct parents to be a
 ![Image of disjoint_sets_path_compression](imgs/disjoint_sets_path_compression.jpg)
 ```
 Code Sample:
@@ -20,14 +44,14 @@ Code Sample:
         public UnionFind(int n){
             count = n;
             parent = new int[n];
-            for(int i = 0;i<n;i++){
-                parent[i] = i;
+            for(int i = 0; i < n; i++){
+                parent[i] = i; //init all node's parent to itself
             }
         }
 
-        // find top parent(the root) which is parent[i] = i
+        // find the leading element in the set
         public int find(int p){
-            while(p!=parent[p]){
+            while(p!=parent[p]){ // leading element => p == parent[p]
                 // path compression
                 parent[p] = parent[parent[p]];
                 p = parent[p];
