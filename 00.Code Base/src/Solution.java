1cfb240d5b43a0cf183fd556f77a6dbf77dc3c7c
IanHe <yinghe.developer@gmail.com>
2020-10-13 05:48:55 -0700

    update UnionFind

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,83 +1,88 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        char[][] charArr = new char[1][2];
-        charArr[0][0] ='a';
-        charArr[0][1] ='a';
-        solution.findWords(charArr, new String[]{"aaa"}).forEach(System.out::println);
+
     }
 
-
-    public List<String> findWords(char[][] board, String[] words) {
-        Trie trie = new Trie();
-        List<String> result = new ArrayList<>();
-        if (board == null || board.length == 0) return result;
-
-        //store words in Trie
-        for (String word : words) trie.insert(word);
-
-        //traverse each cell
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                dfs_backtrack(board, i, j, trie.root, result);
-            }
-        }
-        return result;
-    }
-
-    private void dfs_backtrack(char[][] board, int x, int y, TrieNode node, List<String> result) {
-        //terminator
-        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
-        char ch = board[x][y];
-        // ch == '$' means ch has been visited before
-        if (ch == '$' || node.children[ch - 'a'] == null) return;
-
-        node = node.children[ch - 'a'];
-        //if found a word
-        if (node.word != null) {
-            result.add(node.word);
-            node.word = null;
-        }
-        //traverse four directions, set current to be '$', otherwise child node will traverse back
-        board[x][y] = '$';
-        dfs_backtrack(board, x - 1, y, node, result);
-        dfs_backtrack(board, x + 1, y, node, result);
-        dfs_backtrack(board, x, y - 1, node, result);
-        dfs_backtrack(board, x, y + 1, node, result);
-
-        //set state back
-        board[x][y] = ch;
-    }
-
-    static class TrieNode {
-        public TrieNode[] children;
-        public String word; // store word for leaf node
-
-        public TrieNode() {
-            children = new TrieNode[26];
-        }
-    }
-
-    static class Trie {
-        TrieNode root;
-
-        public Trie() {
-            root = new TrieNode();
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
         }
 
-        public void insert(String word) {
-            TrieNode node = root;
-            for (char ch : word.toCharArray()) {
-                if (node.children[ch - 'a'] == null) {
-                    node.children[ch - 'a'] = new TrieNode();
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                        uf.union(r * nc + c, (r - 1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r + 1][c] == '1') {
+                        uf.union(r * nc + c, (r + 1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c + 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
                 }
-                node = node.children[ch - 'a'];
             }
-            //store word on leaf node
-            node.word = word;
+        }
+
+        return uf.getCount();
+    }
+
+    class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) {
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) {
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
         }
     }
+
+
 }
