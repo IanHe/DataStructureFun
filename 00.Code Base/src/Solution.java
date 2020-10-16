d66288d7a168c390195950e5f1bfc85ac1c45368
IanHe <yhe@bglcorp.com.au>
2020-10-16 18:50:45 +1100

    add more for a*search

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
@@ -8,49 +6,119 @@ public class Solution {
 
     }
 
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>(wordList);
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        // search in set is O(1) faster than list
+        List<List<String>> res = new ArrayList<>();
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return res;
+        }
+        // step 1: use bfs find the shortest path, and record all successors
+        // an Map.Entry<String, Set<String>> is a word can transform to other words in one step change
+        Map<String, Set<String>> successors = new HashMap<>();
+        boolean found = bfs(beginWord, endWord, wordSet, successors);
+        if (!found) {
+            return res;
+        }
+        // step 2: based on the successors, use dfs_backtrack to find all shortest path
+        Deque<String> path = new ArrayDeque<>();
+        path.addLast(beginWord);
+        dfs(beginWord, endWord, successors, path, res);
+        return res;
+    }
 
-        if (set.size() == 0 || !set.contains(endWord)) return 0;
-
+    // two-end bfs
+    private boolean bfs(String beginWord, String endWord, Set<String> wordSet, Map<String, Set<String>> successors) {
+        // record all visited words
         Set<String> visited = new HashSet<>();
-        Set<String> beginSet = new HashSet<>(), endSet = new HashSet<>();
-        beginSet.add(beginWord);
-        endSet.add(endWord);
+        visited.add(beginWord);
+        visited.add(endWord);
 
-        int len = beginWord.length();
-        int step = 1;
+        Set<String> beginVisited = new HashSet<>();
+        beginVisited.add(beginWord);
+        Set<String> endVisited = new HashSet<>();
+        endVisited.add(endWord);
 
-        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
-            if (beginSet.size() > endSet.size()) {
-                Set<String> tmp = beginSet;
-                beginSet = endSet;
-                endSet = tmp;
+        int wordLen = beginWord.length();
+        boolean forward = true; // direction
+        boolean found = false;
+
+        while (!beginVisited.isEmpty() && !endVisited.isEmpty()) {
+            // switch beginVisited and endVisited
+            if (beginVisited.size() > endVisited.size()) {
+                Set<String> temp = beginVisited;
+                beginVisited = endVisited;
+                endVisited = temp;
+
+                // switch direction, for the purpose using successors
+                forward = !forward;
             }
+            Set<String> nextLevelVisited = new HashSet<>();
 
-            Set<String> tmpVisited = new HashSet<>();
-            for (String word : beginSet) {
-                char[] chs = word.toCharArray();
-                for (int i = 0; i < len; i++) {
-                    char origin = chs[i];
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        if (origin == c) continue;
-                        chs[i] = c;
-                        String tmpWord = String.valueOf(chs);
-                        if (set.contains(tmpWord)) {
-                            if (endSet.contains(tmpWord)) return step + 1;
-                            if (!visited.contains(tmpWord)) {
-                                tmpVisited.add(tmpWord);
-                                visited.add(tmpWord);
+            for (String currentWord : beginVisited) {
+                char[] charArray = currentWord.toCharArray();
+                for (int i = 0; i < wordLen; i++) {
+                    char originChar = charArray[i];
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        if (charArray[i] == j) {
+                            continue;
+                        }
+                        charArray[i] = j;
+                        String nextWord = new String(charArray);
+                        if (wordSet.contains(nextWord)) {
+                            if (endVisited.contains(nextWord)) {
+                                found = true;
+                                // add current transformation into successors
+                                addToSuccessors(successors, forward, currentWord, nextWord);
+                            }
+
+                            if (!visited.contains(nextWord)) {
+                                nextLevelVisited.add(nextWord);
+                                // add current transformation into successors
+                                addToSuccessors(successors, forward, currentWord, nextWord);
                             }
                         }
                     }
-                    chs[i] = origin;
+                    charArray[i] = originChar;
                 }
             }
-            beginSet = tmpVisited;
-            step++;
+            beginVisited = nextLevelVisited;
+            visited.addAll(nextLevelVisited);
+            if (found) {
+                break;
+            }
         }
+        return found;
+    }
+
+    // continue traverse beginWord until it meets endWord
+    private void dfs(String beginWord, String endWord, Map<String, Set<String>> successors, Deque<String> path, List<List<String>> res) {
+        if (beginWord.equals(endWord)) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        if (!successors.containsKey(beginWord)) {
+            return;
+        }
+
+        Set<String> successorWords = successors.get(beginWord);
+        for (String successor : successorWords) {
+            path.addLast(successor);
+            dfs(successor, endWord, successors, path, res);
+            path.removeLast();
+        }
+    }
+
+    private void addToSuccessors(Map<String, Set<String>> successors, boolean forward, String currentWord, String nextWord) {
+        if (!forward) {
+            String temp = currentWord;
+            currentWord = nextWord;
+            nextWord = temp;
+        }
+
+        successors.computeIfAbsent(currentWord, a -> new HashSet<>());
+        successors.get(currentWord).add(nextWord);
     }
 
 }
