9aa852153b7c7d467b912df2888efee9e76c9115
IanHe <yinghe.developer@gmail.com>
2020-10-16 17:52:36 -0700

    update bit operation

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,59 +1,46 @@
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[][] grid = new int[][]{{0, 1}, {1, 0}};
     }
 
-    public int slidingPuzzle(int[][] board) {
-        int[][] dir = new int[][]{{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+    public List<List<String>> solveNQueens(int n) {
+        int[] queens = new int[n];
+        Arrays.fill(queens, -1);
+        List<List<String>> solutions = new ArrayList<List<String>>();
+        solve(solutions, queens, n, 0, 0, 0, 0);
+        return solutions;
+    }
 
-        String target = "123456";
-        String start = boardToStr(board);
-
-        Set<String> visited = new HashSet<>();
-        Deque<String> deque = new LinkedList<>();
-        deque.addLast(start);
-        visited.add(start);
-        int step = 1;
-
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            for (int i = 0; i < size; i++) {
-                String str = deque.pollFirst();
-                if (target.equals(str)) return step;
-                int pos = str.indexOf('0');
-                int[] nextMoves = dir[pos];
-                for (int nextPos : nextMoves) {
-                    String newStr = switchChar(str, pos, nextPos);
-                    if (!visited.contains(newStr)) {
-                        visited.add(newStr);
-                        deque.addLast(newStr);
-                    }
-                }
+    public void solve(List<List<String>> solutions, int[] queens, int n, int row, int columns, int diagonals1, int diagonals2) {
+        if (row == n) {
+            List<String> board = generateBoard(queens, n);
+            solutions.add(board);
+        } else {
+            int availablePositions = ((1 << n) - 1) & (~(columns | diagonals1 | diagonals2));
+            while (availablePositions != 0) {
+                int position = availablePositions & (-availablePositions);
+                availablePositions = availablePositions & (availablePositions - 1);
+                int column = Integer.bitCount(position - 1);
+                queens[row] = column;
+                solve(solutions, queens, n, row + 1, columns | position, (diagonals1 | position) << 1, (diagonals2 | position) >> 1);
+                queens[row] = -1;
             }
-            step++;
         }
-        return -1;
     }
 
-    private String switchChar(String str, int pos1, int pos2) {
-        char[] chs = str.toCharArray();
-        char tmp = chs[pos1];
-        chs[pos1] = chs[pos2];
-        chs[pos2] = tmp;
-        return String.valueOf(chs);
+    public List<String> generateBoard(int[] queens, int n) {
+        List<String> board = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            char[] row = new char[n];
+            Arrays.fill(row, '.');
+            row[queens[i]] = 'Q';
+            board.add(new String(row));
+        }
+        return board;
     }
 
-    private String boardToStr(int[][] board) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            sb.append(board[i / 3][i % 3]);
-        }
-        return sb.toString();
-    }
 }
--- 14.Bit Operation/index.md
+++ 14.Bit Operation/index.md
@@ -1,10 +1,28 @@
 ##Bit Operation
 ![Image of why_bit_operation](imgs/why_bit_operation.jpg)
+###How to convert decimal to binary number -> Performing Short Division by Two with Remainder
+![Image of calculate_binary](imgs/calculate_binary.jpg)
+###How to convert binary to decimal number -> Positional Notation or Use Doubling
+![Image of position_notation](imgs/position_notation.jpg) OR <br></br>
+![Image of doubling](imgs/doubling.jpg)<br></br>
+###Bit Operation: 3 * 2 => 3 << 1, 6 / 2 => 6 >> 1
 ![Image of bit_operation](imgs/bit_operation.jpg)
 ![Image of bit_operation_1](imgs/bit_operation_1.jpg)
 ####1s(全1 - all 1)
 ![Image of xor](imgs/xor.jpg)
 ![Image of bit_operation_typicals](imgs/bit_operation_typical.jpg)
+####Note: x&1 => get the last bit; x&(-x) => get the last 1
 ![Image of bit_operation_useful](imgs/bit_operation_useful.jpg)
 ###why n & (n-1) will remove the last 1
 ![Image of n&_n-1](imgs/n&_n-1.jpg)
+<br></br>
+###Leetcode
+####191. Number of 1 Bits - easy - https://leetcode.com/problems/number-of-1-bits/
+####231. Power of Two - easy - https://leetcode.com/problems/power-of-two/
+####190. Reverse Bits - easy - https://leetcode.com/problems/reverse-bits/
+####338. Counting Bits - medium - https://leetcode.com/problems/counting-bits/
+####51. N-Queens - hard - https://leetcode.com/problems/n-queens/
+####52. N-Queens II - hard - https://leetcode.com/problems/n-queens-ii/
+
+
+
