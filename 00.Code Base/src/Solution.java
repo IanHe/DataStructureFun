c6bb51705e7e5a050b488ea2fddd3058a8ae18d9
Ian He <ianhe@ians-imac.home>
2020-11-07 16:01:50 +1100

    update generic&tree recursion

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,49 +1,78 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        TreeNode root = solution.deserialize("1,2,3,null,null,4,5");
+        String str = solution.serialize(root);
+        System.out.println(str);
     }
 
-    public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> list = new ArrayList<>();
-        if (root == null) return list;
-        LinkedList<Node> deque = new LinkedList<>();
-        deque.add(root);
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            List<Integer> tmp = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                Node node = deque.pollFirst();
-                tmp.add(node.val);
-                for (Node child : node.children) {
-                    deque.addLast(child);
-                }
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "";
+        Queue<TreeNode> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) sb.append("null,");
+            else {
+                sb.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
             }
-            list.add(tmp);
         }
-        return list;
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.length() == 0) return null;
+        String[] list = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(list[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while (i < list.length) {
+            TreeNode node = queue.poll();
+            String leftVal = list[i];
+            String rightVal = list[i + 1];
+
+            if (!leftVal.equals("null")) {
+                TreeNode left = new TreeNode(Integer.parseInt(leftVal));
+                node.left = left;
+                queue.offer(left);
+            }
+
+            if (!rightVal.equals("null")) {
+                TreeNode right = new TreeNode(Integer.parseInt(rightVal));
+                node.right = right;
+                queue.offer(right);
+            }
+            i += 2; // every time read 2 nodes - left,right
+        }
+        return root;
     }
 
 
-    class Node {
-        public int val;
-        public List<Node> children;
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-        public Node() {
+        TreeNode() {
         }
 
-        public Node(int _val) {
-            val = _val;
+        TreeNode(int val) {
+            this.val = val;
         }
 
-        public Node(int _val, List<Node> _children) {
-            val = _val;
-            children = _children;
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 
-    ;
 }
---
+++ 05.Generic Recursion & Tree Recursion/index.md
@@ -0,0 +1,30 @@
+##Recursion - DFS
+###Tree Recursion - Preorder(root-left-right), Inorder(left-root-right), Postorder(left-right-root)
+![Image of tree_recursion_code](imgs/tree_recursion_code.jpg)<br></br>
+###Fibonnaci
+![Image of fibonnaci](imgs/fibonnaci.jpg)<br></br>
+![Image of fibonnaci_1](imgs/fibonnaci_1.jpg)<br></br>
+### Code Template:
+```
+function recursion(params){
+    // terminator
+        ...
+    // process current logic
+        ...
+    // drill down
+       recursion(...)
+    // revert state
+       ...
+}
+```
+![Image of generic_recursion](imgs/generic_recursion.jpg)<br></br>
+###Recursion - Always think about code/logic repeatability instead of Brute Force
+![Image of thinking](imgs/thinking.jpg)<br></br>
+###LeetCode
+####70. Climbing Stairs - Easy - https://leetcode.com/problems/climbing-stairs/
+####22. Generate Parentheses - Medium -- https://leetcode.com/problems/generate-parentheses/
+####226. Invert Binary Tree - Easy - https://leetcode.com/problems/invert-binary-tree/description/
+####98. Validate Binary Search Tree - Medium - https://leetcode.com/problems/validate-binary-search-tree/
+####104. Maximum Depth of Binary Tree - easy - https://leetcode.com/problems/maximum-depth-of-binary-tree/
+####111.Minimum Depth of Binary Tree - easy - https://leetcode.com/problems/minimum-depth-of-binary-tree
+####297. Serialize and Deserialize Binary Tree - hard - https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
