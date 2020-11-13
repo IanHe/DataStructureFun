fa59f1d09374d119a18007386e303a1e84fe9a40
IanHe <yhe@bglcorp.com.au>
2020-11-13 16:45:34 +1100

    review dfs

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -1,30 +1,50 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Queue;
+import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> list = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                list.add(node.val);
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (wordList == null || !wordList.contains(endWord)) return 0;
+        Set<String> wordBank = new HashSet<>(wordList);
+        Set<String> visited = new HashSet<>();
+        Set<String> left = new HashSet<>(), right = new HashSet<>();
+        left.add(beginWord);
+        right.add(endWord);
+        int step = 1;
+        while (!left.isEmpty() && !right.isEmpty()) {
+            if (left.size() > right.size()) {
+                Set<String> tmp = left;
+                left = right;
+                right = tmp;
             }
-            res.add(list);
+
+            Set<String> tmpLeft = new HashSet<>();
+            for (String word : left) {
+                char[] chs = word.toCharArray();
+                for (int i = 0; i < chs.length; i++) {
+                    char tmp = chs[i];
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        if(ch == tmp) continue;
+                        chs[i] = ch;
+                        String str = String.valueOf(chs);
+                        if (wordBank.contains(str)) {
+                            if (right.contains(str)) return step + 1;
+                            if (visited.contains(str)) continue;
+                            visited.add(str);
+                            tmpLeft.add(str);
+                        }
+                    }
+                    chs[i] = tmp;
+                }
+            }
+            left = tmpLeft;
+            step++;
         }
-        return res;
+        return 0;
     }
 
 
