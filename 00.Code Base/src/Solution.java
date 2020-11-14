8f3e3b00f4ff17f7b14110d3ba66ef0bf1fd214f
Ian He <ianhe@ians-imac.home>
2020-11-15 10:57:09 +1100

    review and update greedy

--- 00.Code Base/src/Solution.java
+++ 00.Code Base/src/Solution.java
@@ -5,55 +5,16 @@ public class Solution {
         Solution solution = new Solution();
     }
 
-    public char[][] updateBoard(char[][] board, int[] click) {
-        int x = click[0], y = click[1];
-        if (board[x][y] == 'M') {
-            // 规则 1
-            board[x][y] = 'X';
-        } else {
-            bfs(board, x, y);
-        }
-        return board;
-    }
-
-    private void bfs(char[][] board, int posX, int posY) {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        queue.offer(new int[]{posX, posY});
-        visited[posX][posY] = true;
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int cnt = 0, x = pos[0], y = pos[1];
-            // count surrounding mines
-            for (int[] dir : dirs) {
-                int tx = x + dir[0];
-                int ty = y + dir[1];
-                if (tx < 0 || tx >= board.length || ty < 0 || ty >= board[0].length) {
-                    continue;
-                }
-                if (board[tx][ty] == 'M') {
-                    ++cnt;
-                }
-            }
-            if (cnt > 0) {
-                // 规则 3
-                board[x][y] = (char) (cnt + '0');
-            } else {
-                // 规则 2
-                board[x][y] = 'B';
-                for (int[] dir : dirs) {
-                    int tx = x + dir[0];
-                    int ty = y + dir[1];
-                    // 这里不需要在存在 B 的时候继续扩展，因为 B 之前被点击的时候已经被扩展过了
-                    if (tx < 0 || tx >= board.length || ty < 0 || ty >= board[0].length || board[tx][ty] != 'E' || visited[tx][ty]) {
-                        continue;
-                    }
-                    queue.offer(new int[]{tx, ty});
-                    visited[tx][ty] = true;
-                }
+    public int jump(int[] nums) {
+        int step = 0, rightBound = 0, maxPos = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if (i == rightBound) {
+                rightBound = maxPos;
+                step++;
             }
         }
+        return step;
     }
 
     static class TreeNode {
