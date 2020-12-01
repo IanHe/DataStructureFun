b08f4ab598f73e04b9f56f0a25398b8eb32cb313
Ian He <ianhe@ians-imac.home>
2020-12-01 23:37:35 +1100

    review and update bit operation

--- 00.Code Base/test/SolutionTest.java
+++ 00.Code Base/test/SolutionTest.java
@@ -10,12 +10,42 @@ public class SolutionTest {
 }
 
 class Solution {
-    public int reverseBits(int n) {
-        int x = 0;
-        for (int i = 31; n != 0; i--, n = n >>> 1) {
-            x += (n & 1) << i;
+
+    private int count;
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < board.length; i++)
+            Arrays.fill(board[i], '.');
+        dfs(board, 0);
+        return count;
+    }
+
+    private void dfs(char[][] board, int row) {
+        // terminator
+        if (row == board.length) {
+            count++;
+            return;
         }
-        return x;
+
+        for (int i = 0; i < board.length; i++) {
+            if (isValid(board, row, i)) {
+                board[row][i] = 'Q';
+                dfs(board, row + 1);
+                board[row][i] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        // validate top
+        for (int i = 0; i < row; i++) if (board[i][col] == 'Q') return false;
+        // validate top-left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q') return false;
+        // validate top-right
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++)
+            if (board[i][j] == 'Q') return false;
+        return true;
     }
 }
 
