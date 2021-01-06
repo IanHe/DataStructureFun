a8f33fc934f80a268e5bea2c4271aba88868d3be
Ian He <ianhe@Ians-iMac.local>
2021-01-06 18:08:17 +1100

    update advanced dp

--- 00.Code Base/test/SolutionTest.java
+++ 00.Code Base/test/SolutionTest.java
@@ -10,14 +10,33 @@ public class SolutionTest {
     @Test
     public void testSolution() {
         Solution sol = new Solution();
-
+//        assertEquals(sol.lengthOfLIS(new int[]{1, 3, 6, 7, 9, 4, 10, 5, 6}), 6);
     }
 }
 
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-
+    /*
+        if( s(i) == ')' ) =>
+           if( s(i-1) == '(' ) => dp[i] = dp[i]=dp[i−2]+2
+           else if( s[i−dp[i−1]−1]='(' ) => dp[i]=dp[i−1]+dp[i−dp[i−1]−2]+2
+     */
+    public int longestValidParentheses(String s) {
+        int len = 0;
+        char[] chs = s.toCharArray();
+        int[] dp = new int[chs.length];
+        for (int i = 1; i < chs.length; i++) {
+            if (chs[i] == ')') {
+                if (chs[i - 1] == '(') {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && chs[i - dp[i - 1] - 1] == '(') {
+                    dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0);
+                }
+                len = Math.max(len, dp[i]);
+            }
+        }
+        return len;
     }
+
 }
 
 
--- 17.Advanced DP/index.md
+++ 17.Advanced DP/index.md
@@ -33,3 +33,4 @@
 ####746. Min Cost Climbing Stairs - easy - https://leetcode.com/problems/min-cost-climbing-stairs/
 ####72. Edit Distance - hard - https://leetcode.com/problems/edit-distance/
 ####300. Longest Increasing Subsequence - medium - https://leetcode.com/problems/longest-increasing-subsequence/
+######91. Decode Ways - medium - https://leetcode.com/problems/decode-ways/
