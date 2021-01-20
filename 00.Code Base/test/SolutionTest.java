728381d09a625bff949840a66f1846864a5bb311
IanHe <yhe@bglcorp.com.au>
2021-01-21 09:52:37 +1100

    review

--- 00.Code Base/test/SolutionTest.java
+++ 00.Code Base/test/SolutionTest.java
@@ -1,9 +1,5 @@
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.testng.Assert.assertEquals;
 
 public class SolutionTest {
@@ -12,35 +8,28 @@ public class SolutionTest {
         Solution sol = new Solution();
         System.out.println(1 << 1);
 //        assertEquals(sol.myAtoi(" 123"), 123);
+        assertEquals(sol.removeDuplicates(new int[]{1, 1, 1, 2, 2, 2, 3, 3, 3}), 6);
     }
 }
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
+    public int removeDuplicates(int[] nums) {
+        return removeDuplicates(nums, 2);
+    }
+
+    private int removeDuplicates(int[] nums, int k) {
+        if (nums == null) return 0;
+        if (nums.length < k) return nums.length;
+        // because nums is sorted, if nums[i + k] == nums[i] which means nums[i + k] should be removed
         int len = nums.length;
-        if (len < 3) return list;
-        Arrays.sort(nums);
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate
-            int left = i + 1, right = len - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    list.add(List.of(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++; // skip left pointer duplicate
-                    while (left < right && nums[right] == nums[right - 1]) right--; // slip right pointer duplicate
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+        for (int i = 0; i + k < len; i++) {
+            int count = 0;
+            while (nums[i] == nums[i + k]) {
+                nums[i + k] = nums[i + k + (++count)];
+                len--;
             }
         }
-        return list;
+        return len;
     }
 }
 
