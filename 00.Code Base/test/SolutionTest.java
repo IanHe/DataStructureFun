5d66c4bf99e2fb722ba4e9e47d5be38bc7a54918
Ian He <ianhe@ians-imac.home>
2021-01-26 21:28:36 +1100

    review and update

--- 00.Code Base/test/SolutionTest.java
+++ 00.Code Base/test/SolutionTest.java
@@ -1,5 +1,7 @@
 import org.testng.annotations.Test;
 
+import java.util.*;
+
 import static org.testng.Assert.assertEquals;
 
 public class SolutionTest {
@@ -7,30 +9,55 @@ public class SolutionTest {
     public void testSolution() {
         Solution sol = new Solution();
         System.out.println(1 << 1);
-        assertEquals(sol.trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}), 6);
+        Solution.TreeNode node = sol.deserialize("1,2,3,null,null,4,null,null,5");
+        assertEquals(node, null);
     }
 }
 
 class Solution {
-    public int trap(int[] height) {
-        if (height == null || height.length <= 1) return 0;
-        int n = height.length;
-        int left = 0, right = n - 1; // two pointers
-        int leftMax = 0, rightMax = 0, result = 0;
-        while (left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            // min(i_leftMax, i_rightMax) is the matter
-            // if leftMax < rightMax which means leftMax is the min(i_leftMax, i_rightMax) on the left pointer
-            if (leftMax < rightMax) {
-                result += leftMax - height[left]; // leftMax always >= left
-                left++;
-            } else {
-                result += rightMax - height[right];
-                right--;
-            }
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+
+    private void buildString(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null").append(",");
+        } else {
+            sb.append(node.val).append(",");
+            buildString(node.left, sb);
+            buildString(node.right, sb);
         }
-        return result;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Deque<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(",")));
+        return buildTree(nodes);
+    }
+
+    private TreeNode buildTree(Deque<String> nodes) {
+        if(nodes.isEmpty()) return null;
+        String val = nodes.remove();
+        if (val.equals("null")) return null;
+        else {
+            TreeNode node = new TreeNode(Integer.parseInt(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 }
 
