b2166ef31ea099829b1f146ede125182185f7034
Ian He <ianhe@ians-imac.home>
2021-02-04 00:39:46 +1100

    review and update

--- 00.Code Base/test/SolutionTest.java
+++ 00.Code Base/test/SolutionTest.java
@@ -7,23 +7,28 @@ public class SolutionTest {
     public void testSolution() {
         Solution sol = new Solution();
         System.out.println(1 << 1);
-        int result = sol.findMin(new int[]{4, 5, 6, 7, 0, 1, 2});
-        Assert.assertEquals(result, 0);
+        int result = sol.maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4});
+        Assert.assertEquals(result, 6);
     }
 }
 
 class Solution {
-    public int findMin(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        //find the min when left == right
-        while (left < right) {
-            int mid = (left + right) >> 1;
-            if (nums[mid] > nums[right]) { //the minimum on the right
-                left = mid + 1;
-            } else { // the minimum on the left including mid
-                right = mid;
-            }
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) return 0;
+        int len = prices.length;
+        int[] dp = new int[5];
+        dp[1] = -prices[0];
+        dp[2] = Integer.MIN_VALUE;
+        dp[3] = Integer.MIN_VALUE;
+        dp[4] = Integer.MIN_VALUE;
+        for (int i = 1; i < len; i++) {
+            //dp[0] = 0;
+            dp[1] = Math.max(dp[1], dp[0] - prices[i]);
+            dp[2] = Math.max(dp[2], dp[1] + prices[i]);
+            dp[3] = Math.max(dp[3], dp[2] - prices[i]);
+            dp[4] = Math.max(dp[4], dp[3] + prices[i]);
+            System.out.printf(".");
         }
-        return nums[left];
+        return Math.max(0, Math.max(dp[2], dp[4]));
     }
 }
