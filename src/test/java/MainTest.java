e3b46277e1423b727b9309df14eb73717ca87424
IanHe <yinghe.professional@gmail.com>
2021-03-17 01:18:25 +1100

    review & update

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,19 +1,46 @@
 import org.testng.annotations.Test;
 import org.testng.Assert;
 
+import java.util.HashMap;
+
 public class MainTest {
     @Test
     public void test() {
         Solution sol = new Solution();
-        Assert.assertEquals(sol.func(), 1);
+        System.out.println(Integer.pa);
     }
 }
 
 class Solution {
-    public int func() {
-        return 1;
+    public int numDecodings(String s) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return getAns(s.toCharArray(), 0, memo);
     }
 
-
+    private int getAns(char[] chs, int start, HashMap<Integer, Integer> memo) {
+        if (start == chs.length) {
+            return 1;
+        }
+        if (chs[start] == '0') {
+            return 0;
+        }
+        //判断之前是否计算过
+        int m = memo.getOrDefault(start, -1);
+        if (m != -1) {
+            return m;
+        }
+        int ans1 = getAns(chs, start + 1, memo);
+        int ans2 = 0;
+        if (start < chs.length - 1) {
+            int ten = (chs[start] - '0') * 10;
+            int one = chs[start + 1] - '0';
+            if (ten + one <= 26) {
+                ans2 = getAns(chs, start + 2, memo);
+            }
+        }
+        //将结果保存
+        memo.put(start, ans1 + ans2);
+        return ans1 + ans2;
+    }
 
 }
