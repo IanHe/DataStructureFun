6998fe282e636d6a8e46e1ae356381c27e19a712
IanHe <yhe@bglcorp.com.au>
2021-03-25 16:23:49 +1100

    review

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,90 +1,38 @@
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.*;
 
 public class MainTest {
     @Test
     public void test() {
         Solution sol = new Solution();
-        System.out.println(-10 >> 1);
+        System.out.println(sol.isMatch("ab", ".*"));
     }
 }
 
 class Solution {
-    public void solve(char[][] board) {
-        if (board == null || board.length == 0) return;
-        int rows = board.length;
-        int cols = board[0].length;
-        if (cols == 0) return;
-
-        int[][] direction = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        UnionFind set = new UnionFind(rows * cols + 1);
-        int src = rows * cols; // source node for node 'O' on board edges
-
-        // union all 'O' nodes with its surrounding 'O' nodes
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (board[i][j] == 'X') continue;
-                // 将边缘的'O'与超级源点src相连接
-                if (i == 0 || i == rows - 1 || j == 0 || j == cols - 1) {
-                    set.union(src, index(i, j, cols));
-                } else {
-                    // find surround 'O' node and union them
-                    for (int k = 0; k < 4; k++) {
-                        int x = i + direction[k][0];
-                        int y = j + direction[k][1];
-                        if (x >= 0 && y >= 0 && x < rows && y < cols && board[x][y] == 'O')
-                            set.union(index(i, j, cols), index(x, y, cols));
-                    }
-                }
-            }
-        }
-
-        // find 'O' parent node, excluding board edges
-        for (int i = 1; i < rows - 1; i++) {
-            for (int j = 1; j < cols - 1; j++) {
-                if (board[i][j] == 'O' && !set.isConnected(src, index(i, j, cols)))
-                    board[i][j] = 'X';
-            }
-        }
+    // DFS with memo: 1ms
+    public boolean isMatch(String s, String p) {
+        // length + 1, otherwise isMatch("ab", ".*") will throw out of index exception
+        int[][] memo = new int[s.length() + 1][p.length() + 1];
+        return dfs(s.toCharArray(), 0, p.toCharArray(), 0, memo);
     }
 
-    private int index(int x, int y, int cols) {
-        return x * cols + y;
-    }
-
-    static class UnionFind {
-        private int[] parents;
-
-        //init
-        public UnionFind(int n) {
-            this.parents = new int[n];
-            for (int i = 0; i < n; i++) {
-                // parent node are leading node => parent(a) = a
-                parents[i] = i;
-            }
+    private boolean dfs(char[] s, int i, char[] p, int j, int[][] memo) {
+        if (j >= p.length) return i >= s.length; // check boundary, if p and s both reach boundary then return true
+        if (memo[i][j] != 0) return memo[i][j] > 0;
+        // first character match
+        boolean match = i < s.length && (s[i] == p[j] || p[j] == '.');
+        // when p[j+1] == '*'
+        if (j < p.length - 1 && p[j + 1] == '*') {
+            // when don't match suppose '*' match 0 times then p skip to j+2
+            // or p[j] and '*' matches, then s move to i+1
+            boolean t = dfs(s, i, p, j + 2, memo) || (match && dfs(s, i + 1, p, j, memo));
+            memo[i][j] = t ? 1 : -1;
+        }else{
+            // when s[i] match p[j] && s[i+1] matches p[j+1]
+            boolean t = match && dfs(s, i + 1, p, j + 1, memo);
+            memo[i][j] = t ? 1 : -1;
         }
-
-        public boolean isConnected(int x, int y) {
-            return find(x) == find(y);
-        }
-
-        public int find(int x) {
-            while (parents[x] != x) {
-                // route compression
-                parents[x] = parents[parents[x]];
-                x = parents[x];
-            }
-            return x;
-        }
-
-        public void union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) return;
-            parents[rootX] = rootY;
-        }
+        return memo[i][j] == 1;
     }
 }
 
