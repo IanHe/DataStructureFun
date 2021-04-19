c2c0258907f61edee5485e02229e0f426ab5b407
IanHe <yinghe.professional@gmail.com>
2021-04-20 00:49:06 +1000

    review & update

--- src/main/resources/leetcode/09.Binary Search/index.md
+++ src/main/resources/leetcode/09.Binary Search/index.md
@@ -4,10 +4,14 @@
 ![Image of binary_search_code_template](imgs/binary_search_code_template.jpg)
 <br></br>
 <br></br>
-###Leetcode
 ####69. Sqrt(x) - easy - https://leetcode.com/problems/sqrtx/
 ####33. Search in Rotated Sorted Array - medium - https://leetcode.com/problems/search-in-rotated-sorted-array/
 ####74. Search a 2D Matrix - medium - https://leetcode.com/problems/search-a-2d-matrix/
 ####367. Valid Perfect Square - easy - https://leetcode.com/problems/valid-perfect-square/
 ####153. Find Minimum in Rotated Sorted Array - medium - https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 ####34. Find First and Last Position of Element in Sorted Array - medium - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+###On Leetcode Only
+####116. Populating Next Right Pointers in Each Node - medium - https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+####117. Populating Next Right Pointers in Each Node II - medium - https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+####235. Lowest Common Ancestor of a Binary Search Tree - easy - https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,28 +1,45 @@
 import org.testng.annotations.Test;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class MainTest {
     @Test
     public void test() {
         Solution sol = new Solution();
-        System.out.println(sol.isPalindrome("0P"));
+        TreeNode node1 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(6);
+        TreeNode node4 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(1);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node4.left = node6;
+        TreeNode node = sol.lowestCommonAncestor(node1, node6, node5);
+        System.out.println(node.val);
     }
 }
 
 class Solution {
-    public boolean isPalindrome(String s) {
-        char[] arr = s.toLowerCase().toCharArray();
-        int i = 0, j = arr.length - 1;
-        while (i < j) {
-            while (i < j && !Character.isLetterOrDigit(arr[i])) i++;
-            while (i < j && !Character.isLetterOrDigit(arr[j])) j--;
-            if (arr[i] != arr[j]) return false;
-            i++;
-            j--;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        int parentVal = root.val;
+        if (p.val > parentVal && q.val > parentVal) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (p.val < parentVal && q.val < parentVal) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
         }
-        return true;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
     }
 }
 
