7b2bb480a61f1bcabd8f50ecd6dff2727e34a6b1
IanHe <yinghe.professional@gmail.com>
2021-05-16 16:03:15 +1000

    review and update

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,37 +2,51 @@ import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
 public class MainTest {
     @Test
     public void test() {
         Solution sol = new Solution();
-        int[] nums = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-//        sol.bubbleSort(nums);
-//        sol.selectionSort(nums);
-//        sol.insertionSort(nums);
-//        sol.quickSort(nums);
-        sol.mergeSort(nums);
-        Arrays.stream(nums).forEach(num -> System.out.print(num + ", "));
     }
 }
 
 class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        LinkedList<Integer> res = new LinkedList<>();
-        if (root == null) return res;
-        Stack<TreeNode> stack = new Stack<>() {{
-            push(root);
-        }};
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            res.addFirst(node.val);
-            if (node.left != null) stack.push(node.left);
-            if (node.right != null) stack.push(node.right);
+
+    public int minKnightMoves(int x, int y) {
+        int[][] DIRECTIONS = new int[][]{{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+        x = Math.abs(x);
+        y = Math.abs(y);
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        boolean[][] visited = new boolean[305][305];
+        visited[1][1] = true;
+
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] cur = queue.remove();
+                int curX = cur[0];
+                int curY = cur[1];
+                if (curX == x && curY == y) {
+                    return result;
+                }
+
+                for (int[] d : DIRECTIONS) {
+                    int newX = curX + d[0];
+                    int newY = curY + d[1];
+                    if (newX >= -1 && newY >= -1 && !visited[newX + 1][newY + 1]) {
+                        queue.add(new int[]{newX, newY});
+                        visited[newX + 1][newY + 1] = true;
+                    }
+                }
+            }
+            result++;
         }
-        return res;
+        return -1;
     }
 }
 
